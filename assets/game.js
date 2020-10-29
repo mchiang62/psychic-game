df68e4fac231d1cd088c56c408b1acfa3ec19443
Melissa Chiang <mchiang62@gmail.com>
2020-10-29 19:14:24 -0400

    wins & losses work

--- assets/game.js
+++ assets/game.js
@@ -1,13 +1,41 @@
-const alphabet = "abcdefghijklmnopqrstuvwxyz"
+// const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const alphabet = "abc"
 const alphabetArr = alphabet.split("")
-
 console.log(alphabetArr)
+
+
+let wins = 0;
+let losses = 0;
+let guessesRemaining = 10;
+
+
+const winsText = document.getElementById("winCount");
+const lossesText = document.getElementById("lossCount");
+
 
 document.onkeyup = function (event) {
 
-    const userGuess = event.key;
-    const computerGuesses = alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
+    const userGuess = event.key
+    console.log(userGuess + " userguess")
 
+
+    const computerGuesses = alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
     console.log(computerGuesses)
+
+
+    if (userGuess === computerGuesses) {
+        wins++
+    } else if (userGuess !== computerGuesses) {
+        losses++
+    }
+
+
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+
+
+
+
+
 
 }
