289d98127e7e0bcd9c575e2669a3ece3ce2ae3f8
Melissa Chiang <mchiang62@gmail.com>
2020-10-29 18:48:11 -0400

    added some JS

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,13 @@
-console.log("psychic game")
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const alphabetArr = alphabet.split("")
+
+console.log(alphabetArr)
+
+document.onkeyup = function (event) {
+
+    const userGuess = event.key;
+    const computerGuesses = alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
+
+    console.log(computerGuesses)
+
+}
